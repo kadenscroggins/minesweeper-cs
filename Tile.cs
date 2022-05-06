d923d966a41325a593447b1c7b0af8e7905d3a87
Kaden Scroggins <kadenscroggins@gmail.com>
2022-05-06 17:03:45 -0500

    Declared some variables

---
+++ Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper_cs
+{
+    internal struct Tile
+    {
+        public bool isMine;
+        public int bordering;
+        
+        public Tile()
+        {
+            isMine = false;
+            bordering = 0;
+        }
+    }
+}
