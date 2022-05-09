9b3dd8b9278fd92ffc36bafb8c0f731ef12d085a
Kaden Scroggins <kadenscroggins@gmail.com>
2022-05-08 21:17:26 -0500

    Tidied up unused things

--- Minesweeper.cs
+++ Minesweeper.cs
@@ -15,41 +15,33 @@ namespace minesweeper_cs
         {
             ApplicationConfiguration.Initialize();
 
+            // Settings. Need to move to a config file.
             Tile.sizeMultiplier = 30;
             Tile.positionMultiplier = 30;
-
-            GameWindow gameWindow = new();
-
             int mineCount = BeginnerMines;
             int boardWidth = BeginnerBoardWidth;
             int boardHeight = BeginnerBoardHeight;
-            Tile[,] board = new Tile[boardWidth, boardHeight];
 
-            /*TableLayoutPanel mineGrid = new();
-            mineGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            mineGrid.Location = new Point(0, 0);
-            mineGrid.ColumnCount = boardWidth;
-            mineGrid.RowCount = boardHeight;
-            mineGrid.Size = gameWindow.Size;*/
-
+            // Create form and adjust its settings
+            GameWindow gameWindow = new();
             gameWindow.mineGrid.ColumnCount = boardWidth;
             gameWindow.mineGrid.RowCount = boardHeight;
             gameWindow.MaximizeBox = false;
 
+            // Create board and place tiles on it
+            Tile[,] board = new Tile[boardWidth, boardHeight];
             for (int i = 0; i < boardWidth; i++)
             {
                 for (int j = 0; j < boardHeight; j++)
                 {
-                    board[i, j] = new Tile(i, j);
-                    board[i, j].Dock = DockStyle.Fill;
-                    board[i, j].Margin = new Padding(0, 0, 0, 0);
+                    board[i, j] = new Tile
+                    {
+                        Dock = DockStyle.Fill,
+                        Margin = new Padding(0, 0, 0, 0)
+                    };
                     gameWindow.mineGrid.Controls.Add(board[i, j]);
-                    //gameWindow.AddTile(board[i, j]);
                 }
             }
-
-            //gameWindow.Size = new Size(Tile.sizeMultiplier * boardWidth + boardWidth,
-            //    Tile.sizeMultiplier * BeginnerBoardHeight + boardHeight + gameWindow.TitlebarHeight());
 
             Application.Run(gameWindow);
         }
